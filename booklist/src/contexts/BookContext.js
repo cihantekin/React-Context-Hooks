ef686c6c6e65ce27d16665e60111ce10f4c3f08e
Cihan Tekin <cihantekin1990@hotmail.com.tr>
2019-08-29 17:17:32 +0300

    girilen bilgilerin local storegada tutulması ve reducera init olarak local storegadan data verilmesi

--- booklist/src/contexts/BookContext.js
+++ booklist/src/contexts/BookContext.js
@@ -1,12 +1,20 @@
-import React, {createContext, useReducer} from 'react'
+import React, {createContext, useReducer, useEffect} from 'react'
 import { BookReducer } from '../reducers/bookReducer';
 
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-    const [books, dispatch] = useReducer(BookReducer,[]);
+    //use reducer için üçüncü değer init
+    const [books, dispatch] = useReducer(BookReducer,[], () => {
+        const localData = localStorage.getItem('books');
+        return localData ? JSON.parse(localData) : []; 
+    });
 
+    //state her değiştiğinde localstorage da 'books' keyi ile tutulan liste güncellenir
+    useEffect(() => {
+        localStorage.setItem('books', JSON.stringify(books));
+    }, [books])
     return ( 
         <BookContext.Provider value={{books, dispatch}}>
             {props.children}
